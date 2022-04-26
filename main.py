2cc139469893054463af3f74b7938a175a4722dc
jmsmg <jmsmg1@me.com>
2022-04-26 13:16:25 +0900

    mp4 -> mp3 변환 기능 추가

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from pytube import YouTube
+import glob
+import os.path
 
 while True:
     url = input('추출할 URL을 입력하세요. ')
@@ -9,5 +11,15 @@ while True:
 
     # 특정영상 다운로드
     yt.streams.filter(only_audio=True).first().download()
+
+    # 확장자 변경
+    files = glob.glob("*.mp4")
+    for x in files:
+        if not os.path.isdir(x):
+            filename = os.path.splitext(x)
+            try:
+                os.rename(x,filename[0] + '.mp3')
+            except:
+                pass
 
     print('추출 완료')
